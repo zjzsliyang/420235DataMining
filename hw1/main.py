70ba4934dcc9773371ee14341273d53dc21c0b8d
Yang LI <zjzsliyang@gmail.com>
2018-04-08 01:17:28 +0800

    update for test

--- hw1/main.py
+++ hw1/main.py
@@ -3,6 +3,7 @@ import pandas
 import logging
 import matplotlib.pyplot
 from hw1.q1.knn import knn
+import palettable.colorbrewer.qualitative
 from mpl_toolkits.mplot3d import Axes3D
 
 
@@ -27,16 +28,21 @@ def read_data():
     logging.info('DataFrame shape: {}'.format(df.shape))
     logging.debug('DataFrame info: {}'.format(df.info(verbose=False)))
 
-    plot3d(df)
+    plot_raw_data(df)
     return df
 
 
-def plot3d(df):
+def plot_raw_data(df):
+    df.to_csv('res/trade.csv')
     x = []
     y = []
     z = []
     fig = matplotlib.pyplot.figure().gca(projection='3d')
     i = 0
+    les10 = 0
+    les100 = 0
+    les1000 = 0
+    more1000 = 0
     for index, row in df.iterrows():
         for col in df.columns:
             if row[col] != 0:
@@ -45,10 +51,24 @@ def plot3d(df):
                 x.append(i)
                 y.append(i)
                 z.append(row[col])
+                if row[col] < 10:
+                    les10 += 1
+                elif row[col] < 100:
+                    les100 += 1
+                elif row[col] < 1000:
+                    les1000 += 1
+                else:
+                    more1000 += 1
                 i += 1
     fig.scatter(x, y, z)
     logging.info('point No. which are not zero: {}'.format(len(z)))
-    matplotlib.pyplot.savefig('res/raw.jpg', dpi=300)
+    matplotlib.pyplot.savefig('res/raw.jpg', dpi=100)
+
+    matplotlib.pyplot.figure(figsize=(8,6))
+    matplotlib.pyplot.pie([les10, les100, les1000, more1000], colors=palettable.colorbrewer.qualitative.Pastel1_4.mpl_colors, labels=['[0, 10)', '[10, 100)', '[100, 1000)', '[1000, inf)'])
+    matplotlib.pyplot.axis('equal')
+    matplotlib.pyplot.savefig('res/raw2.jpg', dpi=100)
+    matplotlib.pyplot.show()
 
 
 def main():
