a75d1c50df6fe9e73c1a6b8ededd78ff93194e13
Yang LI <zjzsliyang@gmail.com>
2018-05-06 13:08:37 +0800

    fix bugs

--- hw2/main.py
+++ hw2/main.py
@@ -6,8 +6,7 @@ from hw2.src.PrefixSpan import prefixSpan, SquencePattern, print_patterns
 
 
 def find_frequent_patterns(sequential: bool, combined: bool, threshold: int,
-                           item_no: str,
-                           is_new: bool):
+                           item_no: str, is_new: bool):
     df = pandas.read_csv(
         '../data/reco_data/trade_new.csv') if is_new else pandas.read_csv(
         '../data/reco_data/trade.csv')
@@ -18,17 +17,15 @@ def find_frequent_patterns(sequential: bool, combined: bool, threshold: int,
             data = df[[item_no, 'vipno', sldat]].groupby('vipno').apply(
                 lambda x: x.sort_values(by=sldat, ascending=True).head(
                     int(x[item_no].count() * 0.6)))[item_no].groupby(
-                'vipno').apply(
-                set).as_matrix()
+                'vipno').apply(set).as_matrix()
         else:
             data = df[[item_no, 'vipno', sldat]].groupby('vipno').apply(
                 lambda x: x.sort_values(by=sldat, ascending=True).head(
                     int(x[item_no].count() * 0.6))).groupby(sldat)[
-                item_no].apply(
-                set).as_matrix()
+                item_no].apply(set).as_matrix()
         patterns = pyfpgrowth.find_frequent_patterns(data, threshold)
+        logging.info('len of frequent patterns: {}'.format(len(patterns)))
         print(patterns)
-        print(len(patterns))
     else:
         if combined:
             data = df[[item_no, 'vipno', sldat]].groupby('vipno').apply(
@@ -40,12 +37,11 @@ def find_frequent_patterns(sequential: bool, combined: bool, threshold: int,
             data = df[[item_no, 'vipno', sldat]].groupby('vipno').apply(
                 lambda x: x.sort_values(by=sldat, ascending=True).head(
                     int(x[item_no].count() * 0.6))).groupby(sldat)[
-                item_no].apply(
-                set).apply(list).as_matrix()
+                item_no].apply(set).apply(list).as_matrix()
             data = [[[j] for j in i] for i in data]
         patterns = prefixSpan(SquencePattern([], sys.maxsize), data, threshold)
+        logging.info('len of frequent patterns: {}'.format(len(patterns)))
         print_patterns(patterns)
-        print(len(patterns))
 
 
 def main():
